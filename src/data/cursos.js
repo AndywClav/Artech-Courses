8a0e2618a37bbdbcbf42fe403af155665782df07
Andres Clavijo <andresclavijo0822@gmail.com>
2024-04-20 01:47:20 -0300

    test

--- src/data/cursos.js
+++ src/data/cursos.js
@@ -2,7 +2,7 @@ export const CURSOS = [
     {
         id: "1",
         tecnologia: "SQL",
-        logo: "sql.png",
+        logo: "/sql.png",
         nClases: 9,
         author: "Leandro Morrone",
         hrefL: "https://www.linkedin.com/in/leandro-morrone/"
@@ -10,7 +10,7 @@ export const CURSOS = [
     {
         id: "2",
         tecnologia: ".NET",
-        logo: "dotnet.png",
+        logo: "/dotnet.png",
         nClases: 6,
         author: "Leandro Morrone",
         hrefL: "https://www.linkedin.com/in/leandro-morrone/"
@@ -18,7 +18,7 @@ export const CURSOS = [
     {
         id: "3",
         tecnologia: "React",
-        logo: "react.svg",
+        logo: "/react.svg",
         nClases: 6,
         author: "Iván Caravajal \n Gerónimo Riesco",
         hrefL: "https://www.linkedin.com/in/ivan-gabriel-caravajal/ \n https://www.linkedin.com/in/ger%C3%B3nimo-ezequiel-riesco/"
@@ -26,7 +26,7 @@ export const CURSOS = [
     {
         id: "4",
         tecnologia: "SAP",
-        logo: "sap.png",
+        logo: "/sap.png",
         nClases: "?",
         author: "Fernando Caceres \n Fernando Longhi \n Rodolfo Flores \n Mauro Fernandez \n Juan Aleixo",
         hrefL: "https://www.linkedin.com/in/fernando-c%C3%A1ceres-4868bb1b/ \n # \n https://www.linkedin.com/in/rodolfo-flores-28b087a/ \n # \n https://www.linkedin.com/in/juan-manuel-aleixo-52445228/"
@@ -34,7 +34,7 @@ export const CURSOS = [
     {
         id: "5",
         tecnologia: "PowerApps",
-        logo: "powerapps.png",
+        logo: "/powerapps.png",
         nClases: "?",
         author: "Aaron Benavides Vega",
         hrefL: "https://www.linkedin.com/in/aaronvega13/"
@@ -42,7 +42,7 @@ export const CURSOS = [
     {
         id: "6",
         tecnologia: "Recursos",
-        logo: "tools.png",
+        logo: "/tools.png",
         nClases: "7",
         author: "Refuerzo Educativo",
         hrefL: "https://drive.google.com/drive/folders/1KcNO91wZ6sQd-d8o2fmyeO3Y-7oRxDpp"
